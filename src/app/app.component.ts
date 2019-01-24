3be29973e0dcd7e6971d55012cf7fded3361ffd3
iamafsana <afsanaabida@yahoo.com>
2019-01-24 12:09:46 +0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  registrationForm = new FormGroup({
+    userName: new FormControl('Vishwas'),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    // address: new FormGroup({
+    //   city: new FormControl(''),
+    //   state: new FormControl(''),
+    //   postalCode: new FormControl('')
+    // })
+  });
+}
